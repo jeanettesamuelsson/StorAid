3a87c6e4395e48ea2b44a31fa3e5ae8d2b4e29bf
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2025-11-05 10:36:37 +0100

    Add form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,27 +4,55 @@ import './Form.css'
 
 function Form() {
 
-  // add states
+  // states
   const [formData, setFormData] = useState({ name: '', email: '', telephone: '', subject: '', comment: '', })
   const [submitted, setSubmitted] = useState(false)
-  
+  const [errors, setErrors] = useState({})
 
 
-  //function to handle changes in the form. get information from event
   const handleChange = (e) => {
 
-    const {name, value} = e.target
-
-    setFormData({ ...formData, [name]: value })   // (...Spread on formData, updates the value of name, email etc 
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })   // (...Spread, updates the value of name, email etc
 
   }
 
-  //function to handle submit 
+
+  const validateForm = () => {
+
+    const newErrors = {}
+
+    if ((!/^[A-Öa-ö\s\-]{2,}$/.test(formData.name)) || (formData.name === '')) {
+      newErrors.name = true
+    }
+
+    if ((!/^[A-Öa-ö\s\-,]{2,}$/.test(formData.subject)) || (formData.subject === '')) {
+      newErrors.subject = true
+    }
+
+    if ((!/^[A-Öa-ö\s\-,]{5,}$/.test(formData.comment)) || (formData.comment === '')) {
+      newErrors.comment = true
+    }
+
+    if (!/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z0-9]{2,}$/.test(formData.email)) {
+      newErrors.email = true
+    }
+
+    setErrors(newErrors)                  //Set new error to Errors
+    return Object.keys(newErrors).length === 0         //returns true if none of the statements is true 
+
+  }
+
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    //send to api
+
+    if (!validateForm()) {
+      return
+    }
+
+
     const res = await fetch('https://win25-jsf-assignment.azurewebsites.net/api/contact', {
 
       method: 'post',
@@ -35,65 +63,91 @@ function Form() {
 
     })
 
-    // if results ok, set submitted to true and reset the form 
+    console.log(formData)
+
 
     if (res.ok) {
+
       const data = await res.text()
-      console.log(data)
+      console.log('Formulär skickat. API svar:', data)
+
+      // if results ok, set submitted to true and reset the form 
 
       setSubmitted(true)
       setFormData({ name: '', email: '', telephone: '', subject: '', comment: '', })  //reset form
 
 
-      //If results not ok, show json error message
+      //If results not ok, show json error message or text
     } else {
-      const data = await res.json()
-      console.log(data)
+      const errorData = await res.text()
+      console.log('Något blev fel', errorData)
     }
+
+
 
   }
 
- 
+  if (submitted) {
     return (
 
-      <form onSubmit={handleSubmit} noValidate className="contact-form">
+      <div id="submit-message">
+        <h2>
+          Thank you for contacting us!
+        </h2>
+        <p>We will return to you within 2-3 days.</p>
 
-        <div className="input-group">
-          <label htmlFor="name" className="form-label">Your Name</label>
-          <input type="text" name="name" value={formData.name} onChange={handleChange} className="form-input" placeholder="Your Name" required />
-      
-        </div>
+        <button type="button" id="form-btn" onClick={() => setSubmitted(false)} >OK</button>
 
-        <div id="form-row">
-          <div className="input-group">
-            <label htmlFor="email" className="form-label">E-mail</label>
-            <input type="email" name="email" value={formData.email} onChange={handleChange} className="form-input" placeholder="Email" required />
-            
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="telephone" className="form-label">Telephone</label>
-            <input type="text" name="telephone" value={formData.telephone} onChange={handleChange} className="form-input" placeholder="Telephone" />
-            
-          </div>
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="subject" className="form-label">Subject</label>
-          <input type="text" name="subject" value={formData.subject} onChange={handleChange} className="form-input" placeholder="How can we help you" required />
-          
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="comment">Comments/Questions</label>
-          <textarea name="comment" value={formData.comment} onChange={handleChange} className="form-input" rows={8} placeholder="Comment" required />
-          
-        </div>
-
-        <Button id="submit-btn">Submit</Button>
-      </form>
-
+      </div>
     )
   }
 
-  export default Form
+
+  return (
+
+    <form onSubmit={handleSubmit} noValidate className="contact-form">
+
+      <div className="input-group">
+        <label htmlFor="name" className="form-label">Your Name</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange}
+          className={`form-input ${errors.name ? 'error' : ''}`}
+          placeholder="Your Name" required />
+
+      </div>
+
+      <div id="form-row">
+        <div className="input-group">
+          <label htmlFor="email" className="form-label">E-mail</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange}
+            className={`form-input ${errors.email ? 'error' : ''}`} placeholder="Email" required />
+
+        </div>
+
+        <div className='input-group' >
+          <label htmlFor="telephone" className="form-label">Telephone</label>
+          <input type="text" name="telephone" value={formData.telephone} onChange={handleChange} className="form-input" placeholder="Telephone" />
+
+        </div>
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="subject" className="form-label">Subject</label>
+        <input type="text" name="subject" value={formData.subject} onChange={handleChange}
+          className={`form-input ${errors.subject ? 'error' : ''}`} placeholder="How can we help you" required />
+
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="comment">Comments/Questions</label>
+        <textarea name="comment" value={formData.comment} onChange={handleChange}
+          className={`form-input ${errors.comment ? 'error' : ''}`} rows={8} placeholder="Comment" required />
+
+      </div>
+
+      <Button id="submit-btn">Submit</Button>
+    </form>
+
+  )
+}
+
+export default Form
